58a72bb7ff8c95fef13ab3af040dc69af2efe641
Nicholas <91276870+NEOCLOUD0@users.noreply.github.com>
2023-04-13 10:54:57 -0400

    Add files via upload

---
+++ 03. field-validation/javagram/javagram/src/main/java/com/workbook31/javagram/User.java
@@ -0,0 +1,87 @@
+package com.workbook31.javagram;
+
+import com.workbook31.javagram.validation.Age;
+import com.workbook31.javagram.validation.Username;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+public class User {
+    @NotBlank(message = "First name cannot be blank")
+    @Size(min = 2, message = "First name is too short")
+    private String firstName;
+
+    @NotBlank(message = "Last name cannot be blank")
+    @Size(min = 2, message = "First name is too short")
+    private String lastName;
+
+    @Username(message = "Cannot contain special characters or uppercase characters ")
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 7, message = "Username must be at least 7 characters")
+    private String userName;
+
+    @Email(message = "Invalid Email")
+    private String email;
+
+    @Age(message = "Must be at least 18")
+
+    @Past(message = "Date of birth must be in the past")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date dateOfBirth;
+
+
+    public User(String firstName, String lastName, String userName, String email, Date dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public User() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}

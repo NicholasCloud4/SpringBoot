f7bfaa7d0730558bd3aa9ddd2cefc345666f56e1
Nicholas <91276870+NEOCLOUD0@users.noreply.github.com>
2023-05-14 13:38:09 -0400

    Delete 9.sql-database/spring-jpa/starter/grade-submission directory

--- 9.sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ltp.gradesubmission.repository;
-
-import com.ltp.gradesubmission.entity.Grade;
-import org.springframework.data.repository.CrudRepository;
-
-public interface GradeRepository extends CrudRepository<Grade, Long> {
-
-
-}
